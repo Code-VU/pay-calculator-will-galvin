0a44d1968c5444feb980c404bc112bed16e25ef2
Will Galvin <mrc.will.galvin@gmail.com--global user.name Will Galvin>
2022-01-21 17:24:43 -0600

    completed

--- payCalculator.py
+++ payCalculator.py
@@ -3,7 +3,10 @@ def calculatePay():
     print("calculating pay")
     # This first line is provided for you
     hrs = input("Enter Hours:")
+    rate = input("Enter Rate")
     
+    gross_pay = float(hrs) * float(rate)
+    print(gross_pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
